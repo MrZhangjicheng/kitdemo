8732df9b0a751b41fb8be4cb56ba63ee00a00b10
unknown <jicheng.zhang@elco.cn>
2022-08-21 13:58:12 +0800

    添加zap日志以及consul注册实现

---
+++ contrib/log/zap/go.mod
@@ -0,0 +1,10 @@
+module github.com/MrZhangjicheng/kitdemo/contrib/log/zap
+
+go 1.16
+
+require (
+	github.com/MrZhangjicheng/kitdemo v0.0.0
+	go.uber.org/zap v1.22.0
+)
+
+replace github.com/MrZhangjicheng/kitdemo => ../../../
---
+++ contrib/log/zap/zap_test.go
@@ -0,0 +1,47 @@
+package zap
+
+import (
+	"testing"
+
+	"github.com/MrZhangjicheng/kitdemo/log"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+)
+
+type testWriteSyncer struct {
+	output []string
+}
+
+func (x *testWriteSyncer) Write(p []byte) (n int, err error) {
+	x.output = append(x.output, string(p))
+	return len(p), nil
+}
+
+func (x *testWriteSyncer) Sync() error {
+	return nil
+}
+func TestLogger(t *testing.T) {
+	syncer := &testWriteSyncer{}
+	encoderCfg := zapcore.EncoderConfig{
+		MessageKey:     "msg",
+		LevelKey:       "level",
+		NameKey:        "logger",
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.StringDurationEncoder,
+	}
+	core := zapcore.NewCore(zapcore.NewJSONEncoder(encoderCfg), syncer, zap.DebugLevel)
+	zlogger := zap.New(core).WithOptions()
+	logger := NewLogger(zlogger)
+
+	defer func() { _ = logger.Close() }()
+
+	zlog := log.NewHelper(logger)
+
+	zlog.Debugw("log", "debug")
+	zlog.Infow("log", "info")
+	zlog.Warnw("log", "warn")
+	zlog.Errorw("log", "error")
+	zlog.Errorw("log", "error", "except warn")
+
+}
