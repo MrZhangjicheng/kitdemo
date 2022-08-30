1adfd678795c81e76c741ae9a8db73ed78a78437
unknown <jicheng.zhang@elco.cn>
2022-08-30 10:57:57 +0800

    集成http

--- transport/http/server.go
+++ transport/http/server.go
@@ -10,8 +10,8 @@ import (
 
 	"github.com/MrZhangjicheng/kitdemo/internal/endpoint"
 	"github.com/MrZhangjicheng/kitdemo/internal/host"
+	"github.com/MrZhangjicheng/kitdemo/log"
 	"github.com/MrZhangjicheng/kitdemo/transport"
-	"github.com/gorilla/mux"
 )
 
 var (
@@ -19,6 +19,14 @@ var (
 	_ transport.Endpointer = (*Server)(nil)
 	_ http.Handler         = (*Server)(nil)
 )
+
+type ServerOption func(*Server)
+
+func WithHandler(handler http.Handler) ServerOption {
+	return func(s *Server) {
+		s.handler = handler
+	}
+}
 
 // http 服务应该具备的能力
 // 路由注册
@@ -34,29 +42,37 @@ type Server struct {
 	// 是否加密
 	tlsConf *tls.Config
 	// 路由能力 支持动态路由以及路由组的概念
-	router      *mux.Router
-	strictSlash bool // 路由规则是否严格
-
+	// router      *mux.Router
+	// strictSlash bool // 路由规则是否严格
+	handler http.Handler
 }
 
-func NewServer() *Server {
+func NewServer(opts ...ServerOption) *Server {
 	srv := &Server{
-		network:     "tcp",
-		address:     ":0",
-		strictSlash: true,
+		network: "tcp",
+		address: ":0",
+		// strictSlash: true,
 	}
-	srv.router = mux.NewRouter().StrictSlash(srv.strictSlash)
-	srv.router.NotFoundHandler = http.DefaultServeMux
-	srv.router.MethodNotAllowedHandler = http.DefaultServeMux
-	srv.Server = &http.Server{}
+	// srv.router = mux.NewRouter().StrictSlash(srv.strictSlash)
+	// srv.router.NotFoundHandler = http.DefaultServeMux
+	// srv.router.MethodNotAllowedHandler = http.DefaultServeMux
+	for _, o := range opts {
+		o(srv)
+	}
+	if srv.handler == nil {
+		panic("路由未注册")
+	}
+	srv.Server = &http.Server{
+		Handler: srv.handler,
+	}
 	return srv
 
 }
 
 // 路由相关
-func (s *Server) Route(prefix string) *Router {
-	return newRouter(prefix)
-}
+// func (s *Server) Route(prefix string) *Router {
+// 	return newRouter(prefix)
+// }
 
 func (s *Server) Endpoint() (*url.URL, error) {
 	if err := s.listenAndEndpoint(); err != nil {
@@ -75,6 +91,7 @@ func (srv *Server) Start(ctx context.Context) error {
 	if err := srv.listenAndEndpoint(); err != nil {
 		return nil
 	}
+	log.Infof("[http] server listening on: %s", srv.lis.Addr().String())
 	err := srv.Serve(srv.lis)
 	if errors.Is(err, http.ErrServerClosed) {
 		return srv.err
