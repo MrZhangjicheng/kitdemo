8efeac29f2335e7daa8e4093a942e2092e4a9dc9
unknown <jicheng.zhang@elco.cn>
2022-09-16 17:59:27 +0800

    服务端数据

--- transport/grpc/client.go
+++ transport/grpc/client.go
@@ -3,25 +3,60 @@ package grpc
 import (
 	"context"
 	"crypto/tls"
-	"fmt"
-	"net/url"
-	"strings"
+	"log"
+	"time"
 
 	"github.com/MrZhangjicheng/kitdemo/registry"
+	"github.com/MrZhangjicheng/kitdemo/selector"
+	"github.com/MrZhangjicheng/kitdemo/transport/grpc/resolver/discovery"
+
 	"google.golang.org/grpc"
+	"google.golang.org/grpc/credentials"
+	grpcinsecure "google.golang.org/grpc/credentials/insecure"
 )
 
-type Option func(*clientOptions)
+type ClientOption func(*clientOptions)
 
-func WithEndPoint(endpoint string) Option {
+func WithEndPoint(endpoint string) ClientOption {
 	return func(o *clientOptions) {
 		o.endpoint = endpoint
 	}
 }
 
-func Withdiscovery(discovery registry.Discover) Option {
+func WithTimeout(timeout time.Duration) ClientOption {
+	return func(o *clientOptions) {
+		o.timeout = timeout
+	}
+}
+
+func Withdiscovery(discovery registry.Discover) ClientOption {
 	return func(o *clientOptions) {
 		o.discovery = discovery
+	}
+}
+
+// 拦截器
+func WithUnaryInterceptor(in ...grpc.UnaryClientInterceptor) ClientOption {
+	return func(o *clientOptions) {
+		o.ints = in
+	}
+}
+
+// 一些配置
+func WithOptions(opts ...grpc.DialOption) ClientOption {
+	return func(o *clientOptions) {
+		o.grpcOpts = opts
+	}
+}
+
+func WithLogger(log log.Logger) ClientOption {
+	return func(o *clientOptions) {}
+}
+
+// 需要转化成 grpc
+func WithNodeFilter(filters ...selector.NodeFilter) ClientOption {
+	return func(o *clientOptions) {
+		o.filters = filters
 	}
 }
 
@@ -29,96 +64,57 @@ type clientOptions struct {
 	// 客户端基本配置
 	endpoint string // 两种模式 一种是直接ip+port  另一种采用服务发现 "discovery//<author>/servicename"
 	tlsConf  *tls.Config
-	// ints     []grpc.UnaryClientInterceptor
+	// 超时控制
+	timeout time.Duration
+	// 客户端拦截器
+	ints []grpc.UnaryClientInterceptor
+	// grpc 连接参数配置相关
 	grpcOpts  []grpc.DialOption
 	discovery registry.Discover
+
+	balancerName string
+	// 过滤器 中间件
+	filters []selector.NodeFilter
 }
 
-// func dial(ctx context.Context, insecure bool, opts ...clientOptions) (*grpc.ClientConn, error) {
-// 	options := clientOptions{}
-// 	ints := []grpc.UnaryClientInterceptor{}
-// 	if len(options.ints) > 0 {
-// 		ints = append(ints, options.ints...)
-// 	}
-// 	grpcOpts := []grpc.DialOption{
-// 		grpc.WithChainUnaryInterceptor(ints...),
-// 	}
-// 	if options.discovery != nil {
-// 		grpcOpts = append(grpcOpts, grpc.WithResolvers(
-// 			discovery.NewBuilder(
-// 				options.discovery,
-// 				// discovery.WithInsecure(insecure),
-// 			)))
-// 	}
-// 	if options.tlsConf != nil {
-// 		grpcOpts = append(grpcOpts, grpc.WithTransportCredentials(credentials.NewTLS(options.tlsConf)))
-// 	}
-// 	if len(options.grpcOpts) > 0 {
-// 		grpcOpts = append(grpcOpts, options.grpcOpts...)
-// 	}
-// 	return grpc.DialContext(ctx, options.endpoint, grpcOpts...)
-// }
+// Dial 返回 grpc 的 连接
+func Dial(ctx context.Context, opts ...ClientOption) (*grpc.ClientConn, error) {
+	return dial(ctx, false, opts...)
+}
 
-func Dial(ctx context.Context, opts ...Option) (*grpc.ClientConn, error) {
-	options := clientOptions{}
-	grpcOpts := []grpc.DialOption{
-		grpc.WithInsecure(),
+func dial(ctx context.Context, insecure bool, opts ...ClientOption) (*grpc.ClientConn, error) {
+	options := clientOptions{
+		timeout: 2000 * time.Microsecond,
 	}
-	for _, opt := range opts {
-		opt(&options)
+	for _, o := range opts {
+		o(&options)
+	}
+	// 将自定义的中间件,过滤器 装华为 grpc的拦截器
+	ints := []grpc.UnaryClientInterceptor{}
+	if len(options.ints) > 0 {
+		ints = append(ints, options.ints...)
+	}
+	grpcOpts := []grpc.DialOption{
+		// 负载均衡相关配置
+		// grpc.WithDefaultServiceConfig(fmt.Sprintf(`{"loadBalacncingConfig":[{"%s":{}}]}`, options.balancerName)),
+
+		grpc.WithChainUnaryInterceptor(ints...),
+	}
+	if options.discovery != nil {
+		grpcOpts = append(grpcOpts, grpc.WithResolvers(
+			discovery.NewBuilder(
+				options.discovery,
+				discovery.WithInsecure(insecure),
+			)))
+	}
+	if insecure {
+		grpcOpts = append(grpcOpts, grpc.WithTransportCredentials(grpcinsecure.NewCredentials()))
+	}
+	if options.tlsConf != nil {
+		grpcOpts = append(grpcOpts, grpc.WithTransportCredentials(credentials.NewTLS(options.tlsConf)))
 	}
 	if len(options.grpcOpts) > 0 {
 		grpcOpts = append(grpcOpts, options.grpcOpts...)
 	}
-	// 判断 endpoint 传入的是 服务的地址还是注册中心的地址
-
-	target, err := parseTarget(options.endpoint)
-	if err != nil {
-		return nil, err
-	}
-	if target.Scheme == "grpc" && target.Authority != "" {
-		return grpc.DialContext(ctx, options.endpoint, grpcOpts...)
-	}
-
-	if options.discovery != nil {
-		watcher, err := options.discovery.Watch(context.Background(), target.Endpoint)
-		if err != nil {
-			panic("服务发现错误")
-		}
-		srvs, _ := watcher.Next()
-		// 同时开启http服务和grpc服务选择对服务
-		srv := srvs[0]
-		for _, k := range srv.Endpoints {
-			if strings.Contains(k, "grpc") {
-				options.endpoint = k[7:]
-			}
-		}
-	}
-
-	fmt.Println(options.endpoint)
 	return grpc.DialContext(ctx, options.endpoint, grpcOpts...)
-
-}
-
-type Target struct {
-	Scheme    string
-	Authority string
-	Endpoint  string
-}
-
-func parseTarget(endpoint string) (*Target, error) {
-	if !strings.Contains(endpoint, "://") {
-		endpoint = "grpc://" + endpoint
-	}
-
-	u, err := url.Parse(endpoint)
-	if err != nil {
-		return nil, err
-	}
-	target := &Target{Scheme: u.Scheme, Authority: u.Host}
-	if len(u.Path) > 1 {
-		target.Endpoint = u.Path[1:]
-	}
-
-	return target, nil
 }
--- transport/grpc/resolver/discovery/builder.go
+++ transport/grpc/resolver/discovery/builder.go
@@ -1,6 +1,8 @@
 package discovery
 
 import (
+	"time"
+
 	"github.com/MrZhangjicheng/kitdemo/registry"
 
 	"google.golang.org/grpc/resolver"
@@ -8,19 +10,56 @@ import (
 
 const name = "discovery"
 
-type builder struct {
-	discover registry.Discover
+type Option func(o *builder)
+
+func WithTimeout(timeout time.Duration) Option {
+	return func(o *builder) {
+		o.timeout = timeout
+	}
 }
 
-func NewBuilder(d registry.Discover) resolver.Builder {
-	b := &builder{
-		discover: d,
+func WithInsecure(insecure bool) Option {
+	return func(o *builder) {
+		o.insecure = insecure
 	}
+}
 
+func DisableDebugLog() Option {
+	return func(o *builder) {
+		o.debugLogDisabled = true
+	}
+}
+
+type builder struct {
+	discover         registry.Discover
+	timeout          time.Duration
+	insecure         bool
+	debugLogDisabled bool
+}
+
+func NewBuilder(d registry.Discover, opts ...Option) resolver.Builder {
+	b := &builder{
+		discover:         d,
+		timeout:          time.Second * 10,
+		insecure:         false,
+		debugLogDisabled: false,
+	}
+	for _, o := range opts {
+		o(b)
+	}
 	return b
 }
 
 func (b *builder) Build(target resolver.Target, cc resolver.ClientConn, opts resolver.BuildOptions) (resolver.Resolver, error) {
+	// watchRes := &struct {
+	// 	err error
+	// 	w   registry.Watcher
+	// }{}
+	// done := make(chan struct{}, 1)
+	// ctx, cancel := context.WithCancel(context.Background())
+	// go func() {
+
+	// }
 	return nil, nil
 
 }
